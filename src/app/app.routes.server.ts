f27710aaa98823ef11365993a8401c27898cca98
Hossam <hossamm258987@gmail.com>
2026-09-19 15:11:43 +0300

    Try-4

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,11 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'article/:slug',
+    renderMode: RenderMode.Server, // Renders on the server on request instead of prerendering at build time
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender, // Prerenders static routes like /home and /about
+  },
 ];
